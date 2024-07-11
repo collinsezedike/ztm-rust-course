31b9331f24ad189e026aecef31309bdf2625d374
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2024-07-11 17:01:52 +0100

    complete: activity 1

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,20 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+// * Use a function to display your first name
+fn print_first_name() {
+    // * Use the println macro to display messages to the terminal
+    println!("Collins");
+}
+// * Use a function to display your last name
+fn print_last_name() {
+    // * Use the println macro to display messages to the terminal
+    println!("Ezedike-egwom");
+}
+
+
+fn main() {
+    print_first_name();
+    print_last_name();
+}
