40902a3f074c40bf9363f7b44727ced01b031223
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2024-07-12 08:56:59 +0100

    complete: activity 7

--- src/bin/a7.rs
+++ src/bin/a7.rs
@@ -10,4 +10,27 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Colour {
+    Red,
+    Blue,
+    Green,
+    Black,
+    White,
+}
+
+
+fn print_colour(colour: Colour) {
+    match colour {
+        Colour::Red => println!("colour is red"),
+        Colour::Blue => println!("colour is blue"),
+        Colour::Green => println!("colour is green"),
+        Colour::Black => println!("colour is black"),
+        Colour::White => println!("colour is white"),
+    };
+}
+
+
+fn main() {
+    let my_colour = Colour::Black;
+    print_colour(my_colour);
+}
