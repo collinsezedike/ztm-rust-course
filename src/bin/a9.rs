9eff652a40fe1a04011bb2636b0e98e913b30461
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2024-07-12 10:01:23 +0100

    complete: activity 9

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,19 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn get_coordinates() -> (i32, i32) {
+    (3, 2)
+}
+
+
+fn main() {
+    let (x, y) = get_coordinates();
+
+    if y > 5 {
+        println!("y is greater than 5");
+    } else if y < 5 {
+        println!("y is less than 5");
+    } else {
+        println!("y is equal to 5");
+    }
+}
