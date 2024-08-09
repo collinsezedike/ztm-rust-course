6167e04bd9cd0749d6bced39ad063533cc448e0b
Ezedike-egwom Collins <ezedikecollins@gmail.com>
2024-08-09 16:38:27 +0100

    complete: activity 19

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -13,4 +13,24 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut store = HashMap::new();
+    store.insert("Chairs", 5);
+    store.insert("Beds", 3);
+    store.insert("Tables", 2);
+    store.insert("Couches", 0);
+
+    let mut total_stock = 0;
+
+    for (item, number) in store.iter() {
+        total_stock += number;
+        match number {
+            0 => println!("{:?} are out of stock", item),
+            number => println!("{:?} {:?}", number, item),
+        };
+    };
+
+    println!("There are {:?} items in stock.", total_stock);
+}
